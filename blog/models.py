d5d7568a1a838dcdd0dbe288c3bc15f465238964
Hiroki Nagamine <hirokinagamine@HirokinoMacBook-Pro.local>
2022-04-22 17:38:36 +0900

    コメント機能追加

--- blog/models.py
+++ blog/models.py
@@ -8,3 +8,10 @@ class Post(models.Model):
     intro = models.TextField()
     description = models.TextField()
     posted_date = models.DateTimeField(auto_now_add=True)
+
+class Comment(models.Model):
+    post =models.ForeignKey(Post ,related_name = "comments",on_delete=models.CASCADE) #1対多　on_deleteは関連データを一緒に消す
+    name =models.CharField(max_length=255)
+    email=models.EmailField()
+    description =models.TextField()
+    posted_date = models.DateTimeField(auto_now_add=True)
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,25 @@
 from ssl import HAS_TLSv1_1
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from blog.forms import CommentForm
 from .models import Post
+
 def frontpage(request):
     posts = Post.objects.all()
     return render(request,"blog/frontpage.html",{"posts":posts})#"posts"にposts変数の値を渡す
+
+def post_detail(request,slug):
+    post = Post.objects.get(slug=slug)#引数で渡ってきたslugとmodelのslugの値を照合して一致したらpost変数に値を代入する。
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            
+            return redirect("post_detail",slug=post.slug)
+    else:
+        form = CommentForm()
+        
+    return render(request,"blog/post_detail.html",{"post": post, "form":form})
+#"post"にpost変数の値を渡してhtmlにrenderする
